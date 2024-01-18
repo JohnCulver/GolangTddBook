57b24fec9f3acce84280fb8d8729f8621cafa2a2
joculver2001 <john.culver@nielsen.com>
2024-01-18 16:33:15 -0700

    2 Tests

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("John")
+		want := "Hello, John"
 
+		assertCorrectMessage(got, want, t)
+	})
+	t.Run("Say 'Hello, World' when an empty string is applied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(got, want, t)
+	})
+}
+
+func assertCorrectMessage(got, want string, t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
