8a2628e40c94dcddae12d8b5765f672de50eb2a0
joculver2001 <john.culver@nielsen.com>
2024-01-18 16:47:36 -0700

    Spanish working

--- hello_test.go
+++ hello_test.go
@@ -4,15 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("John")
+		got := Hello("John", "")
 		want := "Hello, John"
 
 		assertCorrectMessage(got, want, t)
 	})
 	t.Run("Say 'Hello, World' when an empty string is applied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(got, want, t)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Juan", spanish)
+		want := "Hola, Juan"
 		assertCorrectMessage(got, want, t)
 	})
 }
